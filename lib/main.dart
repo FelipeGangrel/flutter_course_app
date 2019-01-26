85e182f6b64284ed4352ae222b74824910a30a5b
Felipe Gangrel <felipefrog@gmail.com>
2019-01-26 17:22:35 +0000

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_course_app/product_manager.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
